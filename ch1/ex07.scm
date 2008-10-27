5db4b3286b459cdd8eed0d8fe48a00c575f5bcd2
Paul Barry <mail@paulbarry.com>
2008-10-27 19:39:12 -0400

    Modified clojure and scheme solutions to ex 7 to use nested functions that don't take x as a parameter

--- ch1/ex07.scm
+++ ch1/ex07.scm
@@ -4,24 +4,24 @@
 ;therefore it never terminates
 ;this version works
 
+(define (square x) (* x x))
+
+(define (average x y) (/ (+ x y) 2))
+
 (define (sqrt x)
-  (define (square x) (* x x))
 
-  (define (average x y) (/ (+ x y) 2))
-
-  (define (improve guess x)
-    (average guess (/ x guess)))
-
-  (define (good-enough? guess prev-guess x)
+  (define (good-enough? guess prev-guess)
     (< (abs (/ (- (square guess) (square prev-guess)) guess)) 0.001))
 
-  (define (sqrt-iter guess prev-guess x)
-    (if (good-enough? guess prev-guess x)
-	guess
-	(sqrt-iter (improve guess x) guess
-		   x)))
+  (define (improve guess)
+    (average guess (/ x guess)))
 
-  (sqrt-iter 1.0 0.0 x))
+  (define (sqrt-iter guess prev-guess)
+    (if (good-enough? guess prev-guess)
+	guess
+	(sqrt-iter (improve guess) guess)))
+
+  (sqrt-iter 1.0 0.0))
 
 
 
